dd095fdf6adc05c27dc033ab4e8b79618ad5f3bc
Dev 01 <dev01@Devs-MacBook-Pro.local>
2024-05-26 19:47:18 +0600

    laravel bootcamp completed with blade template engine

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Chirp;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -31,6 +33,12 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+
+    }
 
     /**
      * Get the attributes that should be cast.
